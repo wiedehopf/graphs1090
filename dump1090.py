wiedehopf/graphs1090

commit 381b56e93e800c53fd5c62a0f9ed0ad2d69a3540

improve error handling for the collectd python module

dump1090.py
@@ -56,6 +56,9 @@ def read_stats_1min(instance_name, host, url):
         with closing(urlopen(url + '/data/stats.json', None, 5.0)) as stats_file:
             stats = json.load(stats_file)
     except URLError:
+        collectd.warning(URLError)
+    except:
+        collectd.warning(sys.exc_info()[0])
         return
 
     # Signal measurements - from the 1 min bucket
@@ -148,7 +151,9 @@ def read_stats_1min(instance_name, host, url):
 
             except URLError:
                 collectd.warning(URLError)
-
+            except:
+                collectd.warning(sys.exc_info()[0])
+                return
 
 def read_stats(instance_name, host, url):
     #NaN rrd
@@ -162,6 +167,9 @@ def read_stats(instance_name, host, url):
         with closing(urlopen(url + '/data/stats.json', None, 5.0)) as stats_file:
             stats = json.load(stats_file)
     except URLError:
+        collectd.warning(URLError)
+    except:
+        collectd.warning(sys.exc_info()[0])
         return
 
     # Local message counts
@@ -290,6 +298,9 @@ def read_aircraft(instance_name, host, url):
             aircraft_data = json.load(aircraft_file)
 
     except URLError:
+        collectd.warning(URLError)
+    except:
+        collectd.warning(sys.exc_info()[0])
         return
 
     total = 0
@@ -356,7 +367,10 @@ def read_aircraft_978(instance_name, host, url):
         with closing(urlopen(url + '/data/aircraft.json', None, 5.0)) as aircraft_file:
             aircraft_data = json.load(aircraft_file)
 
-    except URLError as error:
+    except URLError:
+        collectd.warning(URLError)
+    except:
+        collectd.warning(sys.exc_info()[0])
         return
 
     total = 0

commit b135194353781ccb53b270949f341f9f63fa9ce0

fix improved error handling patch
fixes: 381b56e93e800c53fd5c62a0f9ed0ad2d69a3540
that commit wasn't properly tested, this commit fixes hopefully fixes
all problems

dump1090.py
@@ -20,6 +20,7 @@ def handle_config(root):
                     url_978 = ch2.values[0]
             if not url:
                 collectd.warning('No URL found in dump1090 Instance ' + instance_name)
+                return
             else:
                 collectd.register_read(callback=handle_read,
                                        data=(instance_name, urlparse.urlparse(url).hostname, url, url_978),
@@ -31,6 +32,7 @@ def handle_config(root):
 
         else:
             collectd.warning('Ignored config entry: ' + child.key)
+            return
 
 V=collectd.Values(host='', plugin='dump1090', time=0)
 
@@ -55,8 +57,9 @@ def read_stats_1min(instance_name, host, url):
     try:
         with closing(urlopen(url + '/data/stats.json', None, 5.0)) as stats_file:
             stats = json.load(stats_file)
-    except URLError:
-        collectd.warning(URLError)
+    except URLError as error:
+        collectd.warning(str(error))
+        return
     except:
         collectd.warning(sys.exc_info()[0])
         return
@@ -149,8 +152,9 @@ def read_stats_1min(instance_name, host, url):
                            interval = 60)
 
 
-            except URLError:
-                collectd.warning(URLError)
+            except URLError as error:
+                collectd.warning(str(error))
+                return
             except:
                 collectd.warning(sys.exc_info()[0])
                 return
@@ -166,8 +170,9 @@ def read_stats(instance_name, host, url):
     try:
         with closing(urlopen(url + '/data/stats.json', None, 5.0)) as stats_file:
             stats = json.load(stats_file)
-    except URLError:
-        collectd.warning(URLError)
+    except URLError as error:
+        collectd.warning(str(error))
+        return
     except:
         collectd.warning(sys.exc_info()[0])
         return
@@ -297,8 +302,9 @@ def read_aircraft(instance_name, host, url):
         with closing(urlopen(url + '/data/aircraft.json', None, 5.0)) as aircraft_file:
             aircraft_data = json.load(aircraft_file)
 
-    except URLError:
-        collectd.warning(URLError)
+    except URLError as error:
+        collectd.warning(str(error))
+        return
     except:
         collectd.warning(sys.exc_info()[0])
         return
@@ -367,8 +373,9 @@ def read_aircraft_978(instance_name, host, url):
         with closing(urlopen(url + '/data/aircraft.json', None, 5.0)) as aircraft_file:
             aircraft_data = json.load(aircraft_file)
 
-    except URLError:
-        collectd.warning(URLError)
+    except URLError as error:
+        #collectd.warning(str(error))
+        return
     except:
         collectd.warning(sys.exc_info()[0])
         return

install.sh
@@ -104,7 +104,9 @@ mkdir -p /run/graphs1090
 
 systemctl daemon-reload
 systemctl enable collectd &>/dev/null
-systemctl restart collectd lighttpd
+systemctl restart lighttpd
+sleep 1
+systemctl restart collectd
 
 #fix readonly remount logic in fr24feed update script
 sed -i -e 's?$(mount | grep " on / " | grep rw)?{ mount | grep " on / " | grep rw; }?' /usr/lib/fr24/fr24feed_updater.sh &>/dev/null