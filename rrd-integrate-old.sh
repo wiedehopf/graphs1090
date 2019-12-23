eeb2f06580f19b9a7b80360739c20fd069c87a48
Matthias Wirth <matthias.wirth@gmail.com>
2019-12-23 15:22:03 +0100

    add a script to integrate old data

---
+++ rrd-integrate-old.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+date=$(date -I)
+new=/tmp/rrd-tmp
+
+old=$1
+
+if ! [ -d "$old" ]; then
+    echo "Supplied argument is not a directory, exiting!"
+    exit 1
+fi
+if ! [ -d /var/lib/collectd/rrd/localhost ]; then
+    echo "/var/lib/collectd/rrd/localhost not found, exiting!"
+    exit 1
+fi
+
+cd /var/lib/collectd/rrd
+
+systemctl stop collectd
+
+cp -T -r -n localhost $date
+rm -rf $new
+cp -T -r localhost $new
+
+for file in $(cd /var/lib/collectd/rrd/localhost/;find | grep '\.rrd')
+do
+	rrdtool create -r "$new/$file" -r "$old/$file" -t "$new/$file" "localhost/$file"
+done
+
+systemctl start collectd
+systemctl restart graphs1090
+
+echo
+echo
+echo
+echo "All done!"
