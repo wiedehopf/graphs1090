74ff0286de5e60b9140b5e9927ee452bb27167e7
Matthias Wirth <matthias.wirth@gmail.com>
2020-09-13 12:16:06 +0200

    add some completely optional malarky

---
+++ malarky.sh
@@ -0,0 +1,16 @@
+cp -f /usr/share/graphs1090/git/collectd_in_run.service /etc/systemd/system/collectd.service || exit
+sed -i -e 's?DataDir.*?DataDir "/run/collectd"?' /etc/collectd/collectd.conf
+sed -i -e '$d' /etc/default/graphs1090
+echo "DB=/run/collectd" >>/etc/default/graphs1090
+systemctl daemon-reload
+systemctl restart collectd
+systemctl restart graphs1090
+
+cat >/etc/cron.d/collectd_to_disk <<"EOF"
+# restart collectd so data is saved to disk
+42 23 * * * root /bin/systemctl restart collectd
+EOF
+
+echo ---------
+echo the malarky has come to pass!
+echo ---------
