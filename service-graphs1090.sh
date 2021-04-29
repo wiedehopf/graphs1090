422f78f3d94321863a3a6b4695bdca11e9acad76
Matthias Wirth <matthias.wirth@gmail.com>
2021-04-29 08:48:13 +0200

    rework the service script

--- service-graphs1090.sh
+++ service-graphs1090.sh
@@ -10,19 +10,16 @@ graphs() {
 counter=0
 hour_done=0
 
-while sleep 5; do
-	counter=$((counter+1))
-	if [[ $((counter%10)) != 0 ]]; then
-		continue
-	fi
-	m=$(((counter/10)%80))
+while wait;
+do
+    SEC=$(( 10#$(date -u +%S) ))
+    if (( SEC != 45 )); then
+        sleep 0.9 &
+        continue
+    fi
+    sleep 59.99 & # wait in the while condition
 
-	minutes=$(date +%M | sed 's/0\([0-9]\)/\1/')
-	h=$(date +%H)
-
-	if [[ $counter == 1000 ]]; then
-		counter=0
-	fi
+    m=$(( 10#$(date -u +%s) / 60))
 
 	if [[ $((m%5)) == 1 ]]; then
 		graphs 8h
@@ -34,32 +31,26 @@ while sleep 5; do
 		graphs 2h
 	elif [[ $((m%10)) == 5 ]]; then
 		graphs 48h
-	elif [[ $m == 10 ]] || [[ $m == 40 ]] || [[ $m == 70 ]]; then
+	elif [[ $((m%80)) == 30 ]] || [[ $((m%80)) == 70 ]]; then
 		graphs 7d
-	elif [[ $m == 20 ]]; then
+	elif [[ $((m%80)) == 20 ]]; then
 		graphs 14d
-	elif [[ $m == 30 ]]; then
+	elif [[ $((m%80)) == 40 ]]; then
 		graphs 30d
-	elif [[ $m == 50 ]]; then
+	elif [[ $((m%80)) == 50 ]]; then
 		graphs 90d
-	elif [[ $m == 60 ]]; then
+	elif [[ $((m%80)) == 60 ]]; then
 		graphs 180d
+    elif [[ $((m%800)) == 10 ]]; then
+        graphs 365d
+    elif [[ $((m%800)) == 90 ]]; then
+        graphs 730d
+    elif [[ $((m%800)) == 170 ]]; then
+        graphs 1095d
 	fi
 
-	if [[ $minutes == 8 ]] && [[ $hour_done == 0 ]]; then
-		hour_done=1
-		if [[ $h == 01 ]]; then
-			graphs 365d
-		elif [[ $h == 02 ]]; then
-			graphs 730d
-		elif [[ $h == 03 ]]; then
-			graphs 1095d
-		elif [[ $h == 00 ]]; then
-			/usr/share/graphs1090/scatter.sh
-		fi
-	fi
-	if [[ $minutes == 0 ]]; then
-		hour_done=0
-	fi
-
+    if [[ $(date +%H:%M) == 00:07 ]]; then
+        echo running scatter.sh
+        /usr/share/graphs1090/scatter.sh
+    fi
 done
