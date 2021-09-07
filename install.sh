a254fe26495de50865dc362603868e9f15b22956
Matthias Wirth <matthias.wirth@gmail.com>
2021-09-07 08:16:44 +0200

    improve install script resiliency

--- install.sh
+++ install.sh
@@ -8,8 +8,8 @@ repo="https://github.com/wiedehopf/graphs1090"
 ipath=/usr/share/graphs1090
 install=0
 
-commands="git rrdtool collectd"
-packages="git rrdtool collectd-core"
+commands="git rrdtool collectd wget unzip"
+packages="git rrdtool collectd-core wget unzip"
 
 mkdir -p $ipath/installed
 mkdir -p /var/lib/graphs1090/scatter
@@ -80,18 +80,20 @@ fi
 # make sure commands are available if they were just installed
 hash -r
 
+if ! command -v git &>/dev/null || ! command -v wget &>/dev/null || ! command -v unzip &>/dev/null; then
+    apt-get update || true; apt-get install -y --no-install-recommends --no-install-suggests git wget unzip || true
+fi
 function getGIT() {
-    # getGIT $REPO $BRANCH $TARGET-DIR
-    if [[ -z "$1" ]] || [[ -z "$2" ]] || [[ -z "$3" ]]; then
-        echo "getGIT wrong usage, check your script or tell the author!" 1>&2
-        return 1
+    # getGIT $REPO $BRANCH $TARGET (directory)
+    if [[ -z "$1" ]] || [[ -z "$2" ]] || [[ -z "$3" ]]; then echo "getGIT wrong usage, check your script or tell the author!" 1>&2; return 1; fi
+    REPO="$1"; BRANCH="$2"; TARGET="$3"; pushd .; tmp=/tmp/getGIT-tmp.$RANDOM.$RANDOM
+    if cd "$TARGET" &>/dev/null && git fetch --depth 1 origin "$BRANCH" && git reset --hard FETCH_HEAD; then popd && return 0; fi
+    popd; if ! cd /tmp || ! rm -rf "$TARGET"; then return 1; fi
+    if git clone --depth 1 --single-branch --branch "$2" "$1" "$3"; then return 0; fi
+    if wget -O "$tmp" "${REPO%".git"}/archive/$BRANCH.zip" && unzip "$tmp" -d "$tmp.folder"; then
+        if mv -fT "$tmp.folder/$(ls $tmp.folder)" "$TARGET"; then rm -rf "$tmp" "$tmp.folder"; return 0; fi
     fi
-    if ! cd "$3" &>/dev/null || ! git fetch --depth 2 origin "$2" || ! git reset --hard FETCH_HEAD; then
-        if ! rm -rf "$3" || ! git clone --depth 2 --single-branch --branch "$2" "$1" "$3"; then
-            return 1
-        fi
-    fi
-    return 0
+    rm -rf "$tmp" "$tmp.folder"; return 1
 }
 
 if [[ "$1" == "test" ]]
