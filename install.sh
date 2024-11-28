wiedehopf/graphs1090

commit b5fff288d33e49abaa009b45e63fbd6db34a695e

better install for libpython on more debian versions

install.sh
@@ -89,18 +89,32 @@ then
         aptUpdate
 		apt-get install --no-install-suggests --no-install-recommends -y 'libpython2.7' || true
 	fi
+elif grep -E 'bullseye' /etc/os-release -qs; then
+    if ! dpkg -s libpython3.9 2>/dev/null | grep 'Status.*installed' &>/dev/null; then
+		apt-get install --no-install-suggests --no-install-recommends -y 'libpython3.9'
+    fi
 elif grep -E 'bookworm' /etc/os-release -qs; then
     if ! dpkg -s libpython3.11 2>/dev/null | grep 'Status.*installed' &>/dev/null; then
 		apt-get install --no-install-suggests --no-install-recommends -y 'libpython3.11'
     fi
+elif grep -E 'trixie' /etc/os-release -qs; then
+    if ! dpkg -s libpython3.12 2>/dev/null | grep 'Status.*installed' &>/dev/null; then
+		apt-get install --no-install-suggests --no-install-recommends -y 'libpython3.12'
+    fi
 else
     if ! dpkg -s libpython3.9 2>/dev/null | grep 'Status.*installed' &>/dev/null \
-        && ! dpkg -s libpython3.10 2>/dev/null | grep 'Status.*installed' &>/dev/null
+        && ! dpkg -s libpython3.10 2>/dev/null | grep 'Status.*installed' &>/dev/null \
+        && ! dpkg -s libpython3.11 2>/dev/null | grep 'Status.*installed' &>/dev/null \
+        && ! dpkg -s libpython3.12 2>/dev/null | grep 'Status.*installed' &>/dev/null \
+        && ! dpkg -s libpython3.13 2>/dev/null | grep 'Status.*installed' &>/dev/null
 	then
         aptUpdate
 
-		apt-get install --no-install-suggests --no-install-recommends -y 'libpython3.9' \
-		|| apt-get install --no-install-suggests --no-install-recommends -y 'libpython3.10'
+		apt-get install --no-install-suggests --no-install-recommends -y 'libpython3.9'
+		apt-get install --no-install-suggests --no-install-recommends -y 'libpython3.10'
+		apt-get install --no-install-suggests --no-install-recommends -y 'libpython3.11'
+		apt-get install --no-install-suggests --no-install-recommends -y 'libpython3.12'
+		apt-get install --no-install-suggests --no-install-recommends -y 'libpython3.13'
 	fi
 fi
 

commit 6be5eb9587f16d0747c5e753d8b76cb439ecaba0

Fix: Whole script would fail if any package could not be found (e.g. libpython3.9)
Fix bug introduced in  by b5fff288d33e49abaa009b45e63fbd6db34a695e
whereby the whole script would fail if any package could not be found (e.g. libpython3.9).

install.sh
@@ -109,12 +109,11 @@ else
         && ! dpkg -s libpython3.13 2>/dev/null | grep 'Status.*installed' &>/dev/null
 	then
         aptUpdate
-
-		apt-get install --no-install-suggests --no-install-recommends -y 'libpython3.9'
-		apt-get install --no-install-suggests --no-install-recommends -y 'libpython3.10'
-		apt-get install --no-install-suggests --no-install-recommends -y 'libpython3.11'
-		apt-get install --no-install-suggests --no-install-recommends -y 'libpython3.12'
-		apt-get install --no-install-suggests --no-install-recommends -y 'libpython3.13'
+		   apt-get install --no-install-suggests --no-install-recommends -y 'libpython3.9'  \
+		|| apt-get install --no-install-suggests --no-install-recommends -y 'libpython3.10' \
+		|| apt-get install --no-install-suggests --no-install-recommends -y 'libpython3.11' \
+		|| apt-get install --no-install-suggests --no-install-recommends -y 'libpython3.12' \
+		|| apt-get install --no-install-suggests --no-install-recommends -y 'libpython3.13'
 	fi
 fi
 