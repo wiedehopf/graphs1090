f464b96195cb8ccd066638c74231ce533f492631
Matthias Wirth <matthias.wirth@gmail.com>
2026-03-09 14:27:05 +0000

    be smarter about guessing which disk statistics to display
    this avoids displaying stats for old disks which are in the rrd data

--- graphs1090.sh
+++ graphs1090.sh
@@ -131,7 +131,7 @@ fi
 if [[ -n $disk ]]; then
     disk="disk-${disk}"
 else
-    disk="$(ls ${DB}/localhost | grep disk -m1)"
+    disk="$(grep -F -o -m 1 -e mmcblk0 -e mmcblk1 -e sda -e hda -e nvme0n1 </proc/diskstats || ls ${DB}/localhost | grep disk -m1)"
 fi
 
 
